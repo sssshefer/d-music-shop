363478b2bae4d4ea385c680e4d77b1e2aeb8824e
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-08-17 12:13:12 +0300

    connectWallet function

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # d-music-shop
 Decentralized music shop on Ethereum blockchain
 
+Run contracts
 '''bash
 npx hardhat run deploy/deploy.ts --network localhost
+'''
+
+Generate backend abi for typescript
+'''bash
+npx typechain --target ethers-v6 --out-dir ./front/src/typechain "artifacts/contracts/**/*[!dbg].json"
 '''
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,113 +1,96 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect, FormEvent } from "react";
+
+import { ethers } from "ethers";
+import { MusicShop__factory } from "@/typechain";
+import type { Album, MusicShop } from "@/typechain";
+
+import type { BrowserProvider } from "ethers";
+
+import ConnectWallet from "@/components/ConnectWallet";
+import WaitingForTransactionMessage from "@/components/WaitingForTransactionMessage";
+import TransactionErrorMessage from "@/components/TransactionErrorMessage";
+
+const HARDHAT_NETWORK_ID = "0x539";
+const MUSIC_SHOP_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+
+declare let window: any;
+
+type CurrentConnectionProps = {
+  provider: BrowserProvider | undefined;
+  shop: MusicShop | undefined;
+  signer: ethers.JsonRpcSigner | undefined;
+};
 
 export default function Home() {
+  const [networkError, setNetworkError] = useState<string>();
+  const [currentConnection, setCurrentConnection] = useState<CurrentConnectionProps>();
+
+  const _connectWallet = async () => {
+    //check if metamask is installed in browser
+    if (window.ethereum === undefined) {
+      setNetworkError("Please install Metamask!");
+
+      return;
+    }
+
+    //check Hardhat network is used
+    if (!(await _checkNetwork())) {
+      return;
+    }
+
+    //get accounts from Metamask
+    const [selectedAccount] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    await _initialize(ethers.getAddress(selectedAccount));
+
+    window.ethereum.on(
+      "accountsChanged",
+      async ([newAccount]: [newAccount: string]) => {
+        if (newAccount === undefined) {
+          return _resetState();
+        }
+
+        await _initialize(ethers.getAddress(newAccount));
+      }
+    )
+
+    window.ethereum.on("chainChanged", ([_networkId]: any) => {
+      _resetState();
+    })
+  }
+
+  async function _checkNetwork(): Promise<boolean> {
+    const chosenChainId = await window.ethereum.request({
+      method: "eth_chainId"
+    });
+
+    if (chosenChainId === HARDHAT_NETWORK_ID)
+      return true;
+
+    setNetworkError("Please connect to Hardhat network (localhost:8545)!");
+
+    return false;
+  }
+
+  const _initialize = async (selectedAccount: string) => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner(selectedAccount);
+
+    setCurrentConnection({
+      ...currentConnection,
+      provider,
+      signer,
+      shop: MusicShop__factory.connect(MUSIC_SHOP_ADDRESS, signer),
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
